80ad9b6e3ddc23370a7adea2e0e89cf4f95ed9b0
sergk <drizer84@gmail.com>
2024-07-21 16:35:29 +0300

    homework2  to check

--- home_work2/src/lib/unique_ptr.h
+++ home_work2/src/lib/unique_ptr.h
@@ -1,7 +1,38 @@
 #ifndef MY_UNIQUE_PTR
 #define MY_UNIQUE_PTR
+
+
 namespace hmwrk2{
+
+template <class T>
+class unique_ptr{
     
+    private:
+        T *raw_ptr = nullptr;
+        
+    public:
+        unique_ptr(T* raw_ptr):raw_ptr(raw_ptr)
+        {}
+
+        ~unique_ptr(){ 
+            delete raw_ptr;
+            raw_ptr = nullptr;
+            }
+
+        T* operator -> () const { return raw_ptr; }
+
+        T& operator * () const { return *raw_ptr; }
+
+        T* get() const noexcept { return raw_ptr; }
+
+        T& operator = ( const unique_ptr& r ) = delete;
+
+        explicit operator bool() const noexcept{
+            return raw_ptr != nullptr;
+        }
+};
+
+
 }
 
 
--- home_work2/src/main.cpp
+++ home_work2/src/main.cpp
@@ -1,7 +1,30 @@
-#include<memory>
+#include <cassert>
+#include <utility>
+
+#include "lib/unique_ptr.h"
+
+//using namespace hmwrk2;
+
+
+void test_unique_ptr() {
+    {
+        hmwrk2::unique_ptr<int> testDef1(new int(1));
+        assert(*testDef1 == 1);
+        assert(testDef1);
+        assert(*testDef1.get() == 1);
+        hmwrk2::unique_ptr<int> testDef2(new int(2));
+        //testDef1 = testDef2;
+      
+    }
+        
+}
+
+
+
 
 
 
 int main(int argc, char** argcv){
-    std::unique_ptr<int> ptr = std::unique_ptr<>;
+    test_unique_ptr();
+    
 }
